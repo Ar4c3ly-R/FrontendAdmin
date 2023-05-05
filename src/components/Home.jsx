06f48d9c9df10335e2a7153f82d5829d45e59adf
Aracely Reyes <ara.reyes.0512@gmail.com>
2023-05-04 19:59:29 -0600

    add sidebar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,23 +1,11 @@
+import { Sidebar } from "./sidebar"
 
 export const Home = () => {
   return (
     
     <div className=" h-screen flex justify-center">
-        <div className=" bg-slate-50 h-screen w-screen self-center rounded-md columns-3">
-            <aside>
-                dashboard
-            </aside>
-
-            <main>
-                <section> cards </section>
-                <section> tablas </section>
-                <section> lista </section>
-            </main>
-
-            <article>
-                <section>info datos,</section>
-                <section>perfiles</section>
-            </article>
+        <div className=" bg-slate-50 h-screen w-screen self-center rounded-md ">
+            <Sidebar/>
         </div>
     </div>
   )
